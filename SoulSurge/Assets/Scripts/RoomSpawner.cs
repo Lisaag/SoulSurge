15a1eae76c0157b5197c92a9b01cd35fc3b5a79f
TychoDesktop <TychoZaal@gmail.com>
2019-05-25 14:54:11 +0200

    Dungeon generating working. BUG: Sometimes 2 room spawn on top of each other, making a room inaccessible

---
+++ SoulSurge/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public enum openingDirection
+    {
+        BOTTOM, TOP, LEFT, RIGHT
+    };
+
+    public openingDirection hasToSpawn;
+    private int rand;
+    public bool spawned;
+
+    public Vector3 offset;
+
+    private RoomTemplates roomTemplates;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomTemplates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        if (!spawned)
+        {
+            switch (hasToSpawn)
+            {
+                case openingDirection.BOTTOM:
+                    // Needs to spawn a room with a BOTTOM door
+                    rand = Random.Range(0, roomTemplates.bottomRooms.Length);
+                    Instantiate(roomTemplates.bottomRooms[rand], transform.position + offset, Quaternion.identity);
+                    spawned = true;
+                    break;
+                case openingDirection.TOP:
+                    // Needs to spawn a room with a TOP door
+                    rand = Random.Range(0, roomTemplates.topRooms.Length);
+                    Instantiate(roomTemplates.topRooms[rand], transform.position + offset, Quaternion.identity);
+                    spawned = true;
+                    break;
+                case openingDirection.LEFT:
+                    // Needs to spawn a room with a LEFT door
+                    rand = Random.Range(0, roomTemplates.leftRooms.Length);
+                    Instantiate(roomTemplates.leftRooms[rand], transform.position + offset, Quaternion.identity);
+                    spawned = true;
+                    break;
+                case openingDirection.RIGHT:
+                    // Needs to spawn a room with a RIGHT door
+                    rand = Random.Range(0, roomTemplates.rightRooms.Length);
+                    Instantiate(roomTemplates.rightRooms[rand], transform.position + offset, Quaternion.identity);
+                    spawned = true;
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Spawnpoint"))
+        {
+            if (other.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+            {
+                //  Instantiate(roomTemplates.closedRoom, transform.position + offset, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+        }
+    }
+}
---
+++ SoulSurge/Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+
+    public GameObject closedRoom;
+
+    public List<GameObject> rooms;
+
+    public float waitTimer;
+    private bool spawnedBoss;
+    public GameObject boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitTimer <= 0 && !spawnedBoss)
+        {
+            Instantiate(boss, rooms[rooms.Count - 1].transform.position, Quaternion.identity);
+            spawnedBoss = true;
+        }
+        else
+        {
+            waitTimer -= Time.deltaTime;
+        }
+    }
+}
